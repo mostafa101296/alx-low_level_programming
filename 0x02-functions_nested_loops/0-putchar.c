aa82cc1ac6166e49ffd8ee21bc6b9561a4328af0
Mostafa <ahmed.aak761@gmail.com>
2023-02-21 12:18:18 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,12 @@ char str[] = "Holberton\n";
 int i = 0;
 
 while (str[i] != '\0')
-	{
-	char c = str[i];
+        {
+        char c = str[i];
 
-	_putchar(c);
-	i++;
-	}
+        _putchar(c);
+        i++;
+        }
 return (0);
 }
+
