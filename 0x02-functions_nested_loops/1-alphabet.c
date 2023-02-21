b8163d50f270848513e696393a058d9227a41452
Mostafa <ahmed.aak761@gmail.com>
2023-02-21 13:07:37 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,6 @@
 void print_alphabet(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++) _putchar(letter);
 	_putchar('\n');
 }
