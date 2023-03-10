1f0e20754f868a094cd349435f8b649f5dcf2ad3
Mostafa <ahmed.aak761@gmail.com>
2023-03-10 10:26:31 -0800

    edit one

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,124 +1,27 @@
-#include <stdio.h>
 #include "main.h"
-
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if it works, 1 if there is an ERROR
  */
-
-int _atoi(char *s)
-
+int main(int argc, char **argv)
 {
+	int n, m, diff;
 
-	int i, d, n, len, f, digit;
-
-
-
-	i = 0;
-
-	d = 0;
-
-	n = 0;
-
-	len = 0;
-
-	f = 0;
-
-	digit = 0;
-
-
-
-	while (s[len] != '\0')
-
-		len++;
-
-
-
-	while (i < len && f == 0)
-
-	{
-	    
-		if (s[i] == '-')
-
-			++d;
-
-
-
-		if (s[i] >= '0' && s[i] <= '9')
-
-		{
-
-			digit = s[i] - '0';
-
-			if (d % 2)
-
-				digit = -digit;
-
-			n = n * 10 + digit;
-
-			f = 1;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-
-				break;
-
-			f = 0;
-
-		}
-
-		i++;
-
-	}
-
-
-	if (f == 0)
-
-		return (0);
-
-
-
-	return (n);
-
-}
-
-
-
-/**
-
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-
-int main(int argc, char *argv[])
-
-{
-
-	int result, num1, num2;
-
-
-
-	if (argc < 3 || argc > 3)
-
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-
 	}
 
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	diff = n * m;
 
-	num1 = _atoi(argv[1]);
-
-	num2 = _atoi(argv[2]);
-
-	result = num1 * num2;
-
-	printf("%d\n", result);
+	printf("%i\n", diff);
 
 	return (0);
 }
